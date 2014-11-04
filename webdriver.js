cee264e31dd1e40feff82882145921f3c2527396
sabrinametzger <sabrina.metzger@1und1.de>
2014-11-04 10:56:56 +0100

    changed to webdriver-sync

--- webdriver.js
+++ webdriver.js
@@ -1,4 +1,6 @@
-var webdriver = require('selenium-webdriver');
+
+var wd = require('webdriver-sync');
+var webdrSetup = require('./setupWebdr.js');
 var fs = require('fs');
 var tem;
 var browser;
@@ -11,81 +13,46 @@ var isError = false;
 var url;
 var server;
 var capabilities;
-//  var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 
-process.argv.forEach(function(val, index, array) {
-  url = array[2];
-  server = array[3];
-  capabilities = array[4];
- });
-driver = new webdriver.Builder().
-usingServer(server + '/wd/hub').
-withCapabilities(capabilities).
-build();
-driver.manage().window().maximize();
-driver.get(url).then(function() {
-  //get browser name and version
-  driver.wait(function() {
+driver = webdrSetup.setup(wd);
+wd.wait(function() {
     return driver.executeScript("return window.testsDone");
-  }, 50000);
-  driver.executeScript('return window.navigator.appName').then(function(appName) {
-    driver.executeScript('return window.navigator.userAgent').then(function(userAgent) {
-      var browser = userAgent.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
-      if (browser && (tem = userAgent.match(/version\/([\.\d]+)/i)) !== null) {
-        browser[2] = tem[1];
-      }
-      browser = browser ? [browser[1], browser[2]] : [appName, driver.executeScript('return window.navigator.appVersion').then(function(c) {})];
-      //get test suites
-      driver.findElements(webdriver.By.xpath("//li[contains(@class,'suite')]")).then(function(suites) {
-        suites.forEach(function(suite) {
-          suite.findElement(webdriver.By.xpath("h1/a")).getText().then(function(suitename) {
-            //get tests
-            suite.findElements(webdriver.By.xpath("ul/li")).then(function(tests) {
-              var num = 1;
-              var map = {};
+},{ timeout: 50000, period: 100 });
+ 
+var appName = driver.executeScript('return window.navigator.appName');
+var userAgent = driver.executeScript('return window.navigator.userAgent');
+var browser = userAgent.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
+if (browser && (tem = userAgent.match(/version\/([\.\d]+)/i)) !== null) {
+  browser[2] = tem[1];
+}
+browser = browser ? [browser[1], browser[2]] : [appName, driver.executeScript('return window.navigator.appVersion')];
 
-              tests.forEach(function(test) {
-                test.getAttribute("class").then(function(cl) {
-                  if (cl.indexOf("test pass") != -1) {
-                    result = "ok";
-                    error = test;
+var suites = driver.findElements(wd.By.xpath("//li[contains(@class,'suite')]"));
 
-                  } else {
-                    result = "not ok";
-                    error = test.findElement(webdriver.By.xpath("pre[contains(@class,'error')]"));
-                    isError = true;
-                  }
-                  test.getText().then(function(text) {
-                    error.getText().then(function(errtext) {
-                      if (isError) {
-                        newErr = errtext;
-                        newErr = newErr.replace(/^/g, "\t#");
-                        newErr = newErr.replace(/\n/g, "\n\t#");
-                        text = text.replace(errtext, newErr);
-                      }
-                      if (num == 14) {
-                        debugger;
-                      }
-                      text = text.replace("\n‣", "");
-                      map[num] = text;
-                      tap = tap.concat(result + " " + num + " " + browser[0] + " " + browser[1] + " - " + suitename + " " + map[num] + "\n");
-                      fs.writeFile('result.tap', tap, function(err) {
-                        if (err) throw err;
-                        // console.log('Saved ' + suitename + text);
-                      });
-                      //console.log("tap ", tap);
-                      num++;
-                      isError = false;
-                    });
-                  });
+suites.forEach(function(suite) {
+var suitename = suite.findElement(wd.By.xpath("h1/a")).getText();
+var tests = suite.findElements(wd.By.xpath("ul/li"));
+var num = 1;
+var map = {};
 
-                });
-              });
-            });
-          });
-        });
-      });
-    });
-    driver.quit();
-  });
+tests.forEach(function(test) {
+  var cl = test.getAttribute("class");
+  var text = test.getText();
+    if (cl.indexOf("test pass") != -1) {
+      result = "ok";
+    } else {
+      result = "not ok";
+      error = test.findElement(wd.By.xpath("pre[contains(@class,'error')]")).getText();
+      newErr = error;
+      newErr = newErr.replace(/^/g, "\t#");
+      newErr = newErr.replace(/\n/g, "\n\t#");
+      text = text.replace(error, newErr);
+    }
+    text = text.replace("\n‣", "");
+    map[num] = text;
+    tap = tap.concat(result + " " + num + " " + browser[0] + " " + browser[1] + " - " + suitename + " " + map[num] + "\n");
+    console.log(tap);
+    fs.writeFileSync('result1.tap', tap);
+     num++;
+});
 });
